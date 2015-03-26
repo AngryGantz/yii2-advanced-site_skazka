aad11a1d3b6f7552abdcf4dcb521567390c5d1a7
AngryGantz <AngryGantz@gmail.com>
2015-03-26 19:54:39 +0600

    change footer

--- frontend/views/layouts/_base.php
+++ frontend/views/layouts/_base.php
@@ -105,7 +105,7 @@ $this->registerJsFile(
 
     <footer class="footer">
         <div class="container">
-            <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+            <p class="pull-left">&copy; Сказочный сон <?= date('Y') ?></p>
             <p class="pull-right"><?= Yii::powered() ?></p>
         </div>
     </footer>
